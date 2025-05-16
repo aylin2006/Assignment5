32e02b381e33322a55477e6bcc2c6f1e3902362b
aylin2006 <242480@astanait.edu.kz>
2025-05-16 09:14:41 +0500

    script.js

--- script.js
+++ script.js
@@ -1,104 +1,42 @@
-const BASE_PRICE = 100;
-const EDUCATION_COEFFICIENTS = {
-    'bachelor': 1.5,
-    'college': 1.2,
-    'high_school': 1.05,
-    'middle_school': 0.9
-};
-const NETWORTH_COEFFICIENTS = {
-    'upper_class': 2,
-    'middle_class': 1.5,
-    'lower_class': 1.2
-};
-const CASTE_BONUSES = {
-    'brahmin': 100,
-    'kshatriya': 50,
-    'vaishya': 20,
-    'shudra': 10,
-    'untouchable': -50
-};
-const SKILL_BONUSES = {
-    'music': 10,
-    'cooking': 20,
-    'character': 15,
-    'singing': 10
-};
-const AGE_COEFFICIENTS = {
-    '18-23': 1.5,
-    '24-27': 1.2,
-    '28+': 0.95
-};
-const REPUTATION_COEFFICIENTS = {
-    'parent_gossip': 0.85,
-    'character_gossip': 0.9,
-    'general_gossip': -20
-};
+document.getElementById('submit').addEventListener('click', function () {
+  let price = 100;
 
-function getSelectedValue(id) {
-    return document.getElementById(id).value;
-}
-function isChecked(id) {
-    return document.getElementById(id).checked;
-}
+  const educationFactors = {
+    blank: 1,
+    bachelor: 1.5,
+    college: 1.2,
+    high_school: 1.05,
+    middle_school: 0.9
+  };
+  const education = document.getElementById('education').value;
+  price *= educationFactors[education] || 1;
 
-function getSelectedRadio(name) {
-    const radios = document.getElementsByName(name);
-    for (let radio of radios) {
-        if (radio.checked) {
-            return radio.value;
-        }
-    }
-    return null;
-}
+  const networthFactors = {
+    blank: 1,
+    upper_class: 2,
+    middle_class: 1.5,
+    lower_class: 1.2
+  };
+  const networth = document.getElementById('networth').value;
+  price *= networthFactors[networth] || 1;
 
-function calculatePrice() {
-    let price = BASE_PRICE;
+  const casteValues = {
+    blank: 0,
+    brahmin: 100,
+    kshatriya: 50,
+    vaishya: 20,
+    shudra: 10,
+    untouchable: -50
+  };
+  const caste = document.getElementById('caste').value;
+  price += casteValues[caste] || 0;
 
-    const education = getSelectedValue('education');
-    if (education !== 'blank') {
-        price *= EDUCATION_COEFFICIENTS[education];
-    }
+  const skillValues = { music: 10, cook: 20, easygoing: 15, sing: 10 };
+  document.querySelectorAll('.skill:checked').forEach(skill => {
+    price += skillValues[skill.value] || 0;
+  });
 
-    const networth = getSelectedValue('networth');
-    if (networth !== 'blank') {
-        price *= NETWORTH_COEFFICIENTS[networth];
-    }
-    
-
-    const caste = getSelectedValue('caste');
-    if (caste !== 'blank') {
-        price += CASTE_BONUSES[caste];
-    }
-    
-  
-    for (let skill in SKILL_BONUSES) {
-        if (isChecked(skill)) {
-            price += SKILL_BONUSES[skill];
-        }
-    }
-
-    const age = getSelectedRadio('age');
-    if (age) {
-        price *= AGE_COEFFICIENTS[age];
-    }
-    
- 
-    for (let rep in REPUTATION_COEFFICIENTS) {
-        if (isChecked(rep)) {
-            if (rep === 'general_gossip') {
-                price += REPUTATION_COEFFICIENTS[rep];
-            } else {
-                price *= REPUTATION_COEFFICIENTS[rep];
-            }
-        }
-    }
-    
-    return Math.round(price);
-}
-
-document.getElementById('submit').addEventListener('click', function() {
-    const result = calculatePrice();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `<h3>Final Price: $${result}</h3>`;
-    resultDiv.style.color = result > 0 ? 'green' : 'red';
-}); 
+  const ageRadio = document.querySelector('input[name="age"]:checked');
+  if (ageRadio) {
+    const ageVal = ageRadio.value;
+    if (ageVal === '18-
